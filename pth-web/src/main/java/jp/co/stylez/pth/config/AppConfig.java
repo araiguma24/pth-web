7bf1669c7cbdcfb08d6ac0f3f21683f451b8f474
reito <reito@pyonkichi.flets-east.jp>
2016-11-25 02:21:32 +0900

    propertiesファイルを使ってみる

--- pth-web/src/main/java/jp/co/stylez/pth/config/AppConfig.java
+++ pth-web/src/main/java/jp/co/stylez/pth/config/AppConfig.java
@@ -3,9 +3,11 @@ package jp.co.stylez.pth.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.mvc.Controller;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "jp.co.stylez.pth", excludeFilters = { @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Controller.class) })
 public class AppConfig {
 	
--- pth-web/src/main/java/jp/co/stylez/pth/web/controller/IndexController.java
+++ pth-web/src/main/java/jp/co/stylez/pth/web/controller/IndexController.java
@@ -1,17 +1,37 @@
 package jp.co.stylez.pth.web.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+	@Value("${resoures.greeting:http://workhard.stylez.co.jp.s3-website-ap-northeast-1.amazonaws.com/greeting.json}")
+	Resource resource;
+
 	@RequestMapping("/")
-	public String index(){
+	public String index() throws IOException{
+		try (final InputStream in =this.resource.getInputStream()) {
+			final String content = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
+			System.out.println(content);
+		} 
 		return "index";
 	}
 
 	@RequestMapping("/hoge")
 	public String hoge(){
 		return "hoge";
+	}
+
+	
+	public void accessResource() {
+		
 	}
 }
